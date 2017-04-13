8e20c90d5add4e00cbc6cf118445dac5e9e0b973
Alex <Alex@Alex-PC>
2017-04-13 14:09:21 +0300

    Настройка Spring Security

---
+++ src/main/java/ru/alexander/springsecurity/HomeController.java
@@ -0,0 +1,16 @@
+package ru.alexander.springsecurity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+    
+    @RequestMapping(value = "/", method = RequestMethod.GET)    
+    public String home(){
+        System.out.println("1111");
+        return "home";
+    }
+    
+}
